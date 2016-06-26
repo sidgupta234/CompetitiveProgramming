e2c25c5408f2710bfc339421edcd3ebe233d722a
Siddharth Gupta <siddharthgupta234@gmail.com>
2016-06-26 09:26:09 +0530

    Update CF71A.cpp

--- JuniorTraining/P1A/CF71A.cpp
+++ JuniorTraining/P1A/CF71A.cpp
@@ -23,12 +23,12 @@ int main(){
    	string x;
    	cin>>x;
    	string ans="";
-   	
-   	
+   
    	if(x.length()>10)
-   	cout<<x[0]<<x.length()-2<<x[x.length()-1]<<endl;
-   	else 
-   		cout<<x<<endl;
+   	   cout<<x[0]<<x.length()-2<<x[x.length()-1]<<endl;
+   	
+   	else
+   	    cout<<x<<endl;
    }
 
    return 0;
